e617b52567a0d05ee71bf112741d88a3c4e3a5fb
karolinaskorupska <pixie22@op.pl>
2020-02-26 12:31:54 +0100

    pliki data do podstron trainers i trainings

---
+++ src/data/trainers.js
@@ -0,0 +1,36 @@
+export default [
+
+    {
+    name: "Aneta",
+    surname: "Murawska",
+    photo: "./assets/photo_aneta.png",
+    description: [
+         "Określana przez uczestników szkoleń człowiekiem renesansu, motywacyjnym mówcą, porywa charyzmą, energią,z pasją odkrywa w ludziach ich potencjał i dobre strony. Każdy z warsztatu wychodzi z ogromem inspiracji, motywacji oraz wiary w siebie i swój sukces. Słynie z ogromnej wiedzy, setek biznesowych i osobistych przykładów oraz imponującego doświadczenia. Sama z powodzeniem łączy w swej pracy dokonania artystycznej duszy z temperamentem kierowcy-rajdowca, reporterską przenikliwością i biznesowym talentem.",
+         "Laureatka wielu branżowych nagród, absolwentka Executive Doctoral Business Administration Polskiej Akademii Nauk Instytutu Ekonomicznego – łączone studia doktoranckie i MBA. Ukończyła poznańską Akademię Muzyczną i Uniwersytet im. Adama Mickiewicza, absolwentka dwóch szkół trenerów, właścicielka własnej szkoły trenerów, pomysłodawca pierwszego w Polsce Klubu Networkingu. ",
+         "Właścicielka firmy CreativeMedia wizerunek-doradztwo-szkolenia, uzyskała dyplom ukończenia kursu RTZ (Racjonalnej Terapii Zachowań), managera zarządzania zasobami ludzkimi, dyplom SPMP – Project Manager, dyplom Instytutu Europeistyki w Warszawie – ukończyła kurs na członków rad nadzorczych spółek skarbu państwa i spółek handlowych, ekspert ds. promocji i kontaktów z mediami, dziennikarz, redaktor, wykładowca; pomysłodawczyni i organizatorka pierwszego w Polsce Klubu Networkingu „Poznań mój biznes”, należy do grona ekspertów Wielkopolskiego Związku Pracodawców Prywatnych.",
+
+        ]
+    },
+
+    {
+        name: "Kinga",
+        surname: "Michalak",
+        photo: "./assets/photo_kinga.png",
+        description: [
+            "Prezes firmy Make My Mind sp. z o.o. Specjalizuje się w obszarze strategii rekrutacyjnych i Employer Brandingowych. Od siedmiu lat buduje zespoły IT. Ostatnie cztery lata poświęciła rozwijaniu agencji rekrutacyjnej, starając się odświeżać innowacyjnymi rozwiązaniami obszar Human Resources polskich i zagranicznych organizacji.",
+             "Odpowiedzialna za rozwój takich projektów jak Escape Centre - audyty kompetencyjne dla pracowników poprzez rozwiązywanie zagadek w Escape roomach czy szkoleń w Virtual Reality. Aktualnie równie mocno zaangażowana w rozwijanie start-upu i zarządzanie projektami badawczo - rozwojowymi z obszaru dopasowania pracownika do kultury organizacyjnej pod kątem jego predyspozycji i preferencji. ",
+             "Trenerka kompetencji miękkich z przepracowanymi ponad 800 godzinami na sali szkoleniowej. Efektem doświadczenia edukacyjnego jest jej nowy projekt benefitu szkoleniowego dla pracowników w formie karnetu “Fitness dla umysłu”. Jej domeną jest kreacja połączona z wiedzą psychologiczną i analitycznoartystycznym zacięciem.",
+        ]
+    },
+
+    {
+        name: "Michał",
+        surname: "Łęgowski",
+        photo: "./assets/photo_michal.png",
+        description:[
+            "Jest akredytowanym coachem ACC (International Coaching Federation), trenerem umiejętności społecznych i praktykiem biznesowym. Ukończył Uniwersytet Ekonomiczny w Poznaniu o profilu absolwenta trenera biznesu, posiadam certyfikację VCC (Vocational Competence Certificate) i NSB (Neuro-lingustic programming in Business).",
+             "Jego główne zajęcie to pełnienie funkcji prezesa zarządu spółki – firmy szkoleniowej TrainBrain Polska Sp.z o.o. czyli prowadzenie szkoleń. Ponadto jest mówcą zaawansowanym (certyfikat ACS, ALB)Tostmasters International – międzynarodowej organizacji zajmującej się profesjonalnymi przemówieniami publicznymi. Pełni rolę mentora w AIP inkubatory.pl, gdzie pomaga rozwijać się start-upom i ściśle współpracuje z Business Link, prowadząc szkolenia w formule Business Training.",
+             "Jako przedsiębiorca i biznesmen, wie jak pomagać ludziom w rozwoju osobistym i biznesie. To jego największa pasja od blisko 9 lat. Prywatnie jest ratownikiem WOPR, psychologiem oraz entuzjastą myrmekologii i sportów ekstremalnych.",
+        ]
+    }
+]
---
+++ src/data/trainings.js
@@ -0,0 +1,53 @@
+export default [
+
+    {
+        name:"Treningi dla liderów",
+        price: "0zł",
+        photo: "../assets/DSC0166.png",
+        description:[
+            "– przećwiczysz różne techniki efektywnego zarządzania zespołami",
+            "– dowiesz się jak zbudować i podtrzymać wizerunek skutecznego lidera ",
+            "– pierwsze treningi już we Wrześniu",
+
+        ],
+
+        howManyTrainings:"12 treningów/",
+        howManyHoursEvery:"2 godziny każdy",
+        city:"Poznań",
+        adress: "ul.Towarowa 35", 
+    },
+    {
+        name:"Treningi dla liderów",
+        price: "0zł",
+        photo: "../assets/DSC0166.png",
+        description:[
+            "– przećwiczysz różne techniki efektywnego zarządzania zespołami",
+            "– dowiesz się jak zbudować i podtrzymać wizerunek skutecznego lidera ",
+            "– pierwsze treningi już we Wrześniu",
+
+        ],
+
+        howManyTrainings:"12 treningów/",
+        howManyHoursEvery:"2 godziny każdy",
+        city:"Poznań",
+        adress: "ul.Towarowa 35", 
+    },
+    {
+        name:"Treningi dla liderów",
+        price: "0zł",
+        photo: "../assets/DSC0166.png",
+        description:[
+            "– przećwiczysz różne techniki efektywnego zarządzania zespołami",
+            "– dowiesz się jak zbudować i podtrzymać wizerunek skutecznego lidera ",
+            "– pierwsze treningi już we Wrześniu",
+
+        ],
+
+        howManyTrainings:"12 treningów/",
+        howManyHoursEvery:"2 godziny każdy",
+        city:"Poznań",
+        adress: "ul.Towarowa 35", 
+    }
+
+    
+]
